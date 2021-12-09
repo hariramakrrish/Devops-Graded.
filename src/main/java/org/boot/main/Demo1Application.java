d6cb815e9d2ecfe10c03e9c0964730ef43f365f7
HARIRAMAKRRISHNAN R <hariramakrrishnan.r@hcl.com>
2021-12-10 00:03:49 +0530

    first commit

---
+++ src/main/java/org/boot/main/Demo1Application.java
@@ -0,0 +1,14 @@
+package org.boot.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+		System.out.println("Hello Dev-ops");
+	}
+
+}
